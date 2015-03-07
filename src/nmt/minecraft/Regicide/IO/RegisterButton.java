94ea0a6fe6e40b667bba97e0419e3ab20bcbc4b1
William Fong <trigition@gmail.com>
2015-03-07 16:33:05 -0700

    Edited Main Plugin to register Commands: Regicide

--- src/nmt/minecraft/Regicide/IO/RegisterButton.java
+++ src/nmt/minecraft/Regicide/IO/RegisterButton.java
@@ -9,7 +9,7 @@ public class RegisterButton implements CommandExecutor{
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,
 			String[] arg3) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
