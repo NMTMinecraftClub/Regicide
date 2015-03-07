ab4f9715cd3e817f5a4637f8292cccac7808c185
Skyler <smanzana@nmt.edu>
2015-03-07 14:32:14 -0700

    Added more specifications into the comments of RegicideGame

--- src/nmt/minecraft/Regicide/Game/RegicideGame.java
+++ src/nmt/minecraft/Regicide/Game/RegicideGame.java
@@ -1,5 +1,12 @@
 package nmt.minecraft.Regicide.Game;
 
+/**
+ * A running instance of a regicide game.
+ * <p>Responsible for keeping track of players and scores, starting and stopping the game, etc.</p>
+ * <p>Regicide games should run independently of other regicide game isntances</p>
+ * @author smanzana
+ *
+ */
 public class RegicideGame {
 
 }
