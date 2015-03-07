bb3a2ee253113de8c2d8fad6716ed06d598276f9
Skyler <skymanzanares@hotmail.com>
2015-03-06 17:13:13 -0700

    Created plugin class and made class comment. Also put in skeleton for default methods like onEnable

---
+++ src/nmt/minecraft/Regicide/RegicidePlugin.java
@@ -0,0 +1,34 @@
+package nmt.minecraft.Regicide;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Plugin class for the Regicide game mode.
+ * <p>
+ * This class specifically creates basic game control listeners and a command listener,
+ * but doesn't create an instance of a game or anything else.</p>
+ * @author Skyler, ADD YOUR NAME HERE
+ *
+ */
+public class RegicidePlugin extends JavaPlugin {
+	
+	/**
+	 * Are we gonna have any config options? 
+	 */
+	@Override
+	public void onLoad() {
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	
+}
