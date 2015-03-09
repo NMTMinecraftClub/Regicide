a376246c53a4b47d8f8b9b08d4188c1915d1daf5
Skyler <skymanzanares@hotmail.com>
2015-03-08 22:53:54 -0600

    Removed calls to the RPlayer's villager

--- src/nmt/minecraft/Regicide/Game/Player/RPlayer.java
+++ src/nmt/minecraft/Regicide/Game/Player/RPlayer.java
@@ -35,41 +35,16 @@ public class RPlayer{
 		points = 0;
 		isKing = false;
 		this.player = Bukkit.getPlayer(player);
-		spawnVillager();
 	}
 	
 	public void teleport(Location loc) {
 		player.teleport(loc);
-		villager.teleport(loc);
 	}
 	
 	public Player getPlayer() {
 		return this.player;
 	}
 	
-	/**
-	 * Spawns a villager at the player's position
-	 */
-	private void spawnVillager(){
-		World world = player.getWorld();
-		Location location = player.getLocation();
-		villager = (LivingEntity) world.spawnEntity(location,EntityType.VILLAGER);
-		villager.setHealth(10000000);
-		//TODO: Find some way to make the villager invisible to the player
-	}
-	
-	/**
-	 * moves the villager to sync with the controlling player
-	 */
-	public void syncVillager(){
-		if(villager != null){
-			villager.teleport(player.getLocation());
-			//set the villager target to the player
-			((Creature)villager).setTarget(player);
-			//make the villager invincible
-			villager.setHealth(10000000);
-		}
-	}
 	
 	public void setIsKing(boolean isKing) {
 		this.isKing = isKing;
