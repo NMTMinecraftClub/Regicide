7abbfd7ff936626662903db205d85ff70a49b0cf
Skyler <skymanzanares@hotmail.com>
2015-03-11 21:44:20 -0600

    Updated TODO

--- src/nmt/minecraft/Regicide/RegicidePlugin.java
+++ src/nmt/minecraft/Regicide/RegicidePlugin.java
@@ -16,8 +16,9 @@ import org.bukkit.plugin.java.JavaPlugin;
  * but doesn't create an instance of a game or anything else.</p>
  * @author Skyler, ADD YOUR NAME HERE
  *
- * TODO add food to the king's inventory
  * TODO add villagers! npcs!
+ * TODO players can die by  lava, falling, etc
+ * TODO eating particles!
  */
 public class RegicidePlugin extends JavaPlugin {
 	
